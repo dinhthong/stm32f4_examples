6c317a7a81c15d8430436355c793f3efce900c95
dinh thong <nguyendinhthong@gmail.com>
2018-08-20 12:42:59 +0700

    reading sensors IMU raw projects added

---
+++ hmc5883L_raw/main.c
@@ -0,0 +1,85 @@
+#include "stm32f4xx.h"
+#include "stm32f4xx_it.h"
+#include "HMC5883L.h"
+
+//delay 0.1ms
+void delay_01ms(uint16_t period){
+	
+  	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE);
+  	TIM6->PSC = 8399;		// clk = SystemCoreClock /2 /(PSC+1) = 10KHz
+  	TIM6->ARR = period-1;
+  	TIM6->CNT = 0;
+  	TIM6->EGR = 1;		// update registers;
+  	TIM6->SR  = 0;		// clear overflow flag
+  	TIM6->CR1 = 1;		// enable Timer6
+  	while (!TIM6->SR);
+  	TIM6->CR1 = 0;		// stop Timer6
+  	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, DISABLE);
+}
+void i2c_hmc5883l(void);
+GPIO_InitTypeDef  GPIO_InitStructure;
+int16_t HMC5883Ldata[3];
+int main(void)
+{
+	i2c_hmc5883l();
+	HMC5883L_Initialize();
+
+  /* GPIOD Peripheral clock enable */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+  /* Configure PD12, PD13 in output pushpull mode */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15 ;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+  while (1)
+  {
+		HMC5883L_GetHeading(HMC5883Ldata);
+		delay_01ms(500);
+    GPIO_SetBits(GPIOD,GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
+		delay_01ms(500);
+		GPIO_ResetBits(GPIOD,GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
+  }
+}
+
+
+void i2c_hmc5883l(void)
+{
+#ifdef FAST_I2C_MODE
+ #define I2C_SPEED 400000
+ #define I2C_DUTYCYCLE I2C_DutyCycle_16_9  
+#else /* STANDARD_I2C_MODE*/
+ #define I2C_SPEED 100000
+ #define I2C_DUTYCYCLE I2C_DutyCycle_2
+#endif /* FAST_I2C_MODE*/
+	
+  GPIO_InitTypeDef  GPIO_InitStructure;
+  I2C_InitTypeDef   I2C_InitStructure;
+
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+	
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_7;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;  
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  GPIO_PinAFConfig(GPIOB,GPIO_PinSource6,GPIO_AF_I2C1);
+  GPIO_PinAFConfig(GPIOB,GPIO_PinSource7,GPIO_AF_I2C1);	
+
+  /* I2C De-initialize */
+  I2C_DeInit(I2C1);
+  I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
+  I2C_InitStructure.I2C_DutyCycle = I2C_DUTYCYCLE;
+  I2C_InitStructure.I2C_OwnAddress1 = 0;
+  I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;
+  I2C_InitStructure.I2C_ClockSpeed = I2C_SPEED;
+  I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+  I2C_Init(I2C1, &I2C_InitStructure);
+ /* I2C ENABLE */
+  I2C_Cmd(I2C1, ENABLE); 
+}
---
+++ mpu6050_raw/main.c
@@ -0,0 +1,76 @@
+#include "stm32f4xx.h"
+#include "MPU6050.h"
+#include "HMC5883L.h"
+/*	      MASTER                    
+	 PB8 --- I2C1_SCL              
+	 PB9 --- I2C1_SDA              
+*/
+int16_t MPU6050data[7]; 
+int16_t HMC5883Ldata[3]; 
+
+
+void I2C_Configuration(void);
+void Delay(__IO uint32_t nCount);
+
+int main(void)
+{
+  I2C_Configuration();  
+  MPU6050_Initialize();
+  while (1)
+  {
+     MPU6050_GetRawAccelTempGyro(MPU6050data);
+		Delay(240);
+  }
+}
+
+void I2C_Configuration(void)
+{
+#ifdef FAST_I2C_MODE
+ #define I2C_SPEED 400000
+ #define I2C_DUTYCYCLE I2C_DutyCycle_16_9  
+#else /* STANDARD_I2C_MODE*/
+ #define I2C_SPEED 100000
+ #define I2C_DUTYCYCLE I2C_DutyCycle_2
+#endif /* FAST_I2C_MODE*/
+	
+  GPIO_InitTypeDef  GPIO_InitStructure;
+  I2C_InitTypeDef   I2C_InitStructure;
+
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+	
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;  
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
+	
+  GPIO_PinAFConfig(GPIOB,GPIO_PinSource6,GPIO_AF_I2C1);
+  GPIO_PinAFConfig(GPIOB,GPIO_PinSource7,GPIO_AF_I2C1);	
+
+  /* I2C De-initialize */
+  I2C_DeInit(I2C1);
+  I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
+  I2C_InitStructure.I2C_DutyCycle = I2C_DUTYCYCLE;
+  I2C_InitStructure.I2C_OwnAddress1 = 0;
+  I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;
+  I2C_InitStructure.I2C_ClockSpeed = I2C_SPEED;
+  I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+  I2C_Init(I2C1, &I2C_InitStructure);
+ /* I2C ENABLE */
+  I2C_Cmd(I2C1, ENABLE); 
+  /* Enable Interrupt */
+
+}
+
+
+void Delay(__IO uint32_t nCount)
+{
+  while(nCount--)
+  {
+  }
+}
+
+
+
