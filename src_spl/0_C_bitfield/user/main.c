80776ffd7e3fe607ff1b088d9777ca5a28135eae
dinhthong <nguyendinhthongit@gmail.com>
2021-08-18 16:43:59 +0700

    C bit field: demo accessing hardware register using bit field (blink led)

--- src_spl/0_C_bitfield/user/main.c
+++ src_spl/0_C_bitfield/user/main.c
@@ -14,6 +14,23 @@
 #include <stdlib.h>
 #define PWM_period 20000 // micro seconds
 
+// delay 0.1ms
+void delay_01ms(uint16_t period) {
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE);
+    TIM6->PSC = 8399; // clk = SystemCoreClock /2 /(PSC+1) = 10KHz
+    TIM6->ARR = period - 1;
+    TIM6->CNT = 0;
+    TIM6->EGR = 1; // update registers;
+    TIM6->SR = 0;  // clear overflow flag
+    TIM6->CR1 = 1; // enable Timer6
+    while (!TIM6->SR)
+        ;
+    TIM6->CR1 = 0; // stop Timer6
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, DISABLE);
+}
+GPIO_InitTypeDef GPIO_InitStructure;
+
 typedef struct {
 	unsigned char no0;
 	unsigned char no1;
@@ -55,37 +72,69 @@ typedef struct {
 	unsigned short no15 :1;
 } __BITS16;
 
-// delay 0.1ms
-void delay_01ms(uint16_t period) {
+typedef struct {
+	uint32_t no0 :1;
+	uint32_t no1 :1;
+	uint32_t no2 :1;
+	uint32_t no3 :1;
+	uint32_t no4 :1;
+	uint32_t no5 :1;
+	uint32_t no6 :1;
+	uint32_t no7 :1;
+	uint32_t no8 :1;
+	uint32_t no9 :1;
+	uint32_t no10 :1;
+	uint32_t no11 :1;
+	uint32_t no12 :1;
+	uint32_t no13 :1;
+	uint32_t no14 :1;
+	uint32_t no15 :1;
+	uint32_t no16 :1;
+	uint32_t no17 :1;
+	uint32_t no18 :1;
+	uint32_t no19 :1;
+	uint32_t no20 :1;
+	uint32_t no21 :1;
+	uint32_t no22 :1;
+	uint32_t no23 :1;
+	uint32_t no24 :1;
+	uint32_t no25 :1;
+	uint32_t no26 :1;
+	uint32_t no27 :1;
+	uint32_t no28 :1;
+	uint32_t no29 :1;
+	uint32_t no30 :1;
+	uint32_t no31 :1;
+} __BITS32;
 
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE);
-    TIM6->PSC = 8399; // clk = SystemCoreClock /2 /(PSC+1) = 10KHz
-    TIM6->ARR = period - 1;
-    TIM6->CNT = 0;
-    TIM6->EGR = 1; // update registers;
-    TIM6->SR = 0;  // clear overflow flag
-    TIM6->CR1 = 1; // enable Timer6
-    while (!TIM6->SR)
-        ;
-    TIM6->CR1 = 0; // stop Timer6
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, DISABLE);
-}
+union gpio_reg {
+		uint32_t reg;
+		__BITS32 BIT;
+};
+
 BITS8_t portM;
 /*
 		We use typedef struct __BITS8 to access each bit of an 8-bit register using register.[bit number n]
 		This register can be LED turn on register
 */
-__BITS8* bit8_ptr;
 __BITS8 portA;
 char demo_8bit_reg;
-GPIO_InitTypeDef GPIO_InitStructure;
-#define N 10
+
+#define _8B_REG   (volatile union __BITS8 *)&demo_8bit_reg // Couldn't access using this though
+
+/*
+	Example 2:
+		- We don't need to use pointer anymore, directly define the MACRO that can be used for accessing individual bits of register
+*/
+#define GPIOD_BSRR (*(volatile union gpio_reg *)0x40020C18).reg // access using register name (32bit access)
+#define GPIOD_BSRR_bit (*(volatile union gpio_reg *)0x40020C18).BIT // access individual bit
+
 void init_leds_gpio(void);
 
 int main(void) {
-		demo_8bit_reg = 120;
-		bit8_ptr = (__BITS8 *)&demo_8bit_reg;
+		__BITS8* bit8_ptr = (__BITS8 *)&demo_8bit_reg;
 		init_leds_gpio();
+		demo_8bit_reg = 120;
     printf("Hello this is the start of trace program\r\n");
 
     printf("sizeof(portA) = %d\n\r", sizeof(portA));
@@ -99,9 +148,15 @@ int main(void) {
 		bit8_ptr->no4 = 0;
 		bit8_ptr->no3 = 0;
 		printf("Value of demo_8bit_reg: %d, in hex: 0x%x", demo_8bit_reg, demo_8bit_reg);
+		//*(_8B_REG).no1 = 1;
+		
     while (1) {
-        delay_01ms(5000);
-        GPIO_ToggleBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+			GPIOD_BSRR_bit.no12 = 1;
+			GPIOD_BSRR_bit.no13 = 1;
+      delay_01ms(2000);
+			GPIOD_BSRR_bit.no28 = 1;
+			GPIOD_BSRR_bit.no29 = 1;
+			delay_01ms(2000);
     }
 }
 
--- src_spl/0_C_bitfield/user/readme.md
+++ src_spl/0_C_bitfield/user/readme.md
@@ -1,8 +1,3 @@
 # Brief
-- Keil C project utilizes ITM trace feature. To use the `#include <stdlib.h>` and retargeted printf. I modified retarget file, add definition for `_ttywrch(int ch)` to solve __use_no_semihosting was requested, but _ttywrch was referenced. The solution was found on https://programmersought.com/article/91751264627/
-- This project is a clone from `19-swd_itm_trace`.
-- Using Keil C IDE. Demonstration of ITM trace feature of ST-Link. It's simply just like USART via printf. Project configuration is the most important thing
+- https://aticleworld.com/access-the-port-and-register-using-bit-field-in-embedded-c/
 # How
-To run with ITM trace you have to go Debug session and 
-View -> Serial Window -> Debug (printf) viewer
-Then click Run.
